cd80baea269df25c7d2af7930bdf8ea9ab767018
Blackoutz01 <160240132+DaviSF09@users.noreply.github.com>
2024-05-29 10:26:38 -0300

    TUCA

--- appteste/App.tsx
+++ appteste/App.tsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>tuca donka donkaaaaa dunkeixon</Text>
-      <StatusBar style="auto" />
+      <Text>GIDEON FERAAAAAAAAAAAAAAAAA</Text>      <StatusBar style="auto" />
     </View>
   );
 }
